5341f737c12fbb329501b070a57ce849e42a962f
xinyaoliu <liuxinyao.joanne@gmail.com>
2018-12-20 15:51:04 -0500

    initial commit

---
+++ utils/models.py
@@ -0,0 +1,78 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+from prune import PruningModule, MaskedLinear
+
+
+class LeNet_5(PruningModule):
+    def __init__(self, mask=False):
+        super(LeNet_5, self).__init__()
+        linear = MaskedLinear if mask else Linear
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=(4, 4))
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=(4,4))
+        self.fc1 = linear(120, 84)
+        self.fc2 = linear(84, 10)
+
+    def forward(self, x):
+        # Conv1
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=(2, 2), stride=2)
+
+        # Conv2
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=(2, 2), stride=2)
+
+        # Conv3
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        # Fully-connected
+        x = x.view(-1, 120)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        #x = F.relu(x)
+        #x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+class LeNet_5_CIFAR(PruningModule):
+    def __init__(self, mask=False):
+        super(LeNet_5_CIFAR, self).__init__()
+        linear = MaskedLinear if mask else Linear
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=(5, 5))
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=(5,5))
+        self.fc1 = linear(120, 84)
+        self.fc2 = linear(84, 10)
+        #self.fc3 = linear(48, 10)
+
+    def forward(self, x):
+        # Conv1
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=(2, 2), stride=2)
+
+        # Conv2
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=(2, 2), stride=2)
+
+        # Conv3
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        # Fully-connected
+        x = x.view(-1, 120)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        #x = F.relu(x)
+        #x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
---
+++ utils/prune.py
@@ -0,0 +1,57 @@
+import math
+import numpy as np
+import torch
+from torch.nn import Parameter
+from torch.nn.modules.module import Module
+import torch.nn.functional as F
+
+class PruningModule(Module):
+    def prune_by_std(self, s=0.25):
+        for name, module in self.named_modules():
+            if name in ['fc1', 'fc2', 'fc3']:
+                threshold = np.std(module.weight.data.cpu().numpy()) * s
+                module.prune(threshold)
+
+
+class MaskedLinear(Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(MaskedLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        self.mask = Parameter(torch.ones([out_features, in_features]), requires_grad=False)
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input):
+        return F.linear(input, self.weight * self.mask, self.bias)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+            + 'in_features=' + str(self.in_features) \
+            + ', out_features=' + str(self.out_features) \
+            + ', bias=' + str(self.bias is not None) + ')'
+
+    def prune(self, threshold):
+        weight_dev = self.weight.device
+        mask_dev = self.mask.device
+        # Convert Tensors to numpy and calculate
+        tensor = self.weight.data.cpu().numpy()
+        mask = self.mask.data.cpu().numpy()
+        new_mask = np.where(abs(tensor) < threshold, 0, mask)
+        # Apply new weight and mask
+        self.weight.data = torch.from_numpy(tensor * new_mask).to(weight_dev)
+        self.mask.data = torch.from_numpy(new_mask).to(mask_dev)
+
+
+
+
